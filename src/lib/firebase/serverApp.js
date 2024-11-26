f5213e5d1f8166dfcc1faf04909dce52cded5a66
varjain <varjain@redhat.com>
2024-11-26 18:53:01 +0530

    show signin state

--- src/lib/firebase/serverApp.js
+++ src/lib/firebase/serverApp.js
@@ -7,7 +7,20 @@ import { initializeServerApp } from "firebase/app";
 
 import { firebaseConfig } from "./config";
 import { getAuth } from "firebase/auth";
-
 export async function getAuthenticatedAppForUser() {
-  throw new Error('not implemented');
+  const idToken = headers().get("Authorization")?.split("Bearer ")[1];
+  console.log('firebaseConfig', JSON.stringify(firebaseConfig));
+  const firebaseServerApp = initializeServerApp(
+    firebaseConfig,
+    idToken
+      ? {
+          authIdToken: idToken,
+        }
+      : {}
+  );
+
+  const auth = getAuth(firebaseServerApp);
+  await auth.authStateReady();
+
+  return { firebaseServerApp, currentUser: auth.currentUser };
 }
